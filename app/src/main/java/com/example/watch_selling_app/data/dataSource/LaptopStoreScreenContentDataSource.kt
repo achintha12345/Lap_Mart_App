74f0ecb2604673240326228e6c0fa86222c46fc2
SAMSUNG <thushanachintha15567@gmail.com>
2025-06-09 16:36:52 +0530

    change file name

--- app/src/main/java/com/example/watch_selling_app/data/dataSource/LaptopStoreScreenContentDataSource.kt
+++ app/src/main/java/com/example/watch_selling_app/data/dataSource/LaptopStoreScreenContentDataSource.kt
@@ -1,10 +1,8 @@
 package com.example.watch_selling_app.data.dataSource
 
-import androidx.compose.ui.res.stringResource
-import com.example.watch_selling_app.R
 import com.example.watch_selling_app.domain.model.WatchStoreScreenContent
 
-object WatchStoreScreenContentDataSource {
+object LaptopStoreScreenContentDataSource {
 
     fun getContent(): WatchStoreScreenContent {
         return WatchStoreScreenContent(
